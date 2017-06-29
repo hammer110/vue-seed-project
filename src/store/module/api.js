c42e6d43584afb9464bc73b3374ac817984a9ce2
刘金建 <liujinjian@9fbank.cc>
2017-06-29 21:58:42 +0800

    删掉不必要的信息

--- src/store/module/api.js
+++ src/store/module/api.js
@@ -1,6 +1,5 @@
 // 接口地址列表
 // const domain = process.env.APIPORT
-// **********修改接口的时候一定要注意联合登陆接口和别的接口不一样，一定要注意**********
 const state = {
 }
 
--- src/store/module/userInfo.js
+++ src/store/module/userInfo.js
@@ -1,12 +1,8 @@
 // 用户信息state
-import { getQueryString } from 'lib/until'
 import { UPDATEUSREINFO } from 'store/mutation-types'
 import _ from "lodash"
-const token = getQueryString('userKey') || getQueryString('token') || window.sessionStorage.getItem('token') || ''
 const state = {
-  token,
-  mobile: window.sessionStorage.getItem('mobile') || getQueryString('mobile') || '',
-  uid: window.sessionStorage.getItem('uid') || getQueryString('uid') || ''
+  token: ''
 }
 const mutations = {
   [UPDATEUSREINFO] (state, userInfo) {
